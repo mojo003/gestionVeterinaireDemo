2ca75fe4303c1776d4be69b5a6805ed032f82feb
Mojo <jomoreautrb@gmail.com>
2022-01-10 11:04:46 -0500

    x

---
+++ README.md
@@ -0,0 +1,4 @@
+Description
+
+Gestion Vétérinaire est un prototype créé en MVC dans le cadre d'une formation. Cette application sert à créer des comptes clients, créer des comptes animals relié aux comptes clients du maître et au 
+besoin, trasfèrer l'animal dans une table prêt à être adopté.
--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $dbHost = 'localhost';
 $dbPort = 5432;
 $dbName = 'veterinary';
 $dbUsername = 'postgres';
-$dbPassword = 'Darklloyd03';
+$dbPassword = 'user03';
 $dbOptions = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUsername, $dbPassword, $dbOptions);
